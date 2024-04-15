a157dca8a4872b82e4304abcad25424cb1920225
Pedro-Gattai <pedrogattai09@gmail.com>
2024-04-15 15:30:17 -0300

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule)
   app.enableCors() // CORS habilitado para todas as origens
-  await app.listen(3000)
+  await app.listen(port, '0.0.0.0')
 }
 
 bootstrap()
