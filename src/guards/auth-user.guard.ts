0aa94dcba584990b89560df335968c8a3bf039be
Pedro Gattai <Pedro.Gattai@sou.inteli.edu.br>
2024-05-08 15:49:30 -0300

    uuid verify in console.log

--- src/guards/auth-user.guard.ts
+++ src/guards/auth-user.guard.ts
@@ -1,4 +1,9 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common'
 import { Observable } from 'rxjs'
 import { CheckTokenService } from '../modules/Auth/service/token/check-token.service'
 
@@ -15,6 +20,14 @@ export class AuthGuardUser implements CanActivate {
     try {
       const token = (authorization ?? '').split(' ')[1]
       const data = this.checkTokenService.user(token)
+
+      console.log('Dados Decodificados:', data)
+
+      // Verifica se o campo `uuid` está presente
+      if (!data.uuid) {
+        console.error('UUID não encontrado no token:', data)
+        throw new UnauthorizedException('UUID não encontrado no token')
+      }
 
       request.uuid = data.uuid
       return true
